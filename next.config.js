4cf40fbcf92ea82615a1d1c2f6d3a1d9662c343d
SuryaReddy-5377 <suryareddy5377@gmail.com>
2026-06-19 14:00:27 +0530

    fix: empty sitemap

--- next.config.js
+++ next.config.js
@@ -15,3 +15,15 @@ const nextConfig = {
 }
 
 module.exports = nextConfig
+module.exports = {
+  // ...other config
+  webpack: (config) => {
+    config.resolve.fallback = {
+      ...config.resolve.fallback,
+      fs: false,
+      net: false,
+      tls: false,
+    };
+    return config;
+  },
+}
